41e92214905d7d0e1c4c08ff938766cbf026937f
Patrick Shinn <shinn5112@gmail.com>
2017-04-20 13:32:37 -0400

    Current City

--- app/src/main/java/net/weatheraf/weatherandroidforecast/MainActivity.java
+++ app/src/main/java/net/weatheraf/weatherandroidforecast/MainActivity.java
@@ -60,6 +60,7 @@ public class MainActivity extends AppCompatActivity
         // setup
         sharedPreferences = getSharedPreferences("prefs", MODE_PRIVATE);
         getWeatherData();
+        getSupportActionBar().setTitle("Current City");
 
         // showing home fragment
         fragmentManager.beginTransaction().replace(R.id.main, new home()).commit();
