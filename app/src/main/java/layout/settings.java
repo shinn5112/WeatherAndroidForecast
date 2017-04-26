de5d8d4659b8bd4cdfe12da9193a04d3d5e59479
Brandon <bmduke1997@gmail.com>
2017-04-26 14:28:32 -0400

    keyboard close

--- app/src/main/java/layout/settings.java
+++ app/src/main/java/layout/settings.java
@@ -12,6 +12,7 @@ import android.text.TextWatcher;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.inputmethod.InputMethodManager;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
@@ -110,45 +111,53 @@ public class settings extends Fragment implements AdapterView.OnItemSelectedList
 
     @Override
     public void onClick(View v) {
-        SharedPreferences.Editor prefsEditor = sharedPreferences.edit();
-
-        Float latitudeF = sharedPreferences.getFloat("latitude", 0);
-        Float longitudeF = sharedPreferences.getFloat("longitude", 0);
-
-        //zip code location
-        final Geocoder geocoder = new Geocoder(getActivity());
         final String zip = String.valueOf(zipCode.getText().toString());
+        if (!sharedPreferences.getBoolean("gps", false) && zip.length() == 5) {
+            SharedPreferences.Editor prefsEditor = sharedPreferences.edit();
 
-        try {
-            List<Address> addresses = geocoder.getFromLocationName(zip, 1);
-            if (addresses != null && !addresses.isEmpty()) {
-                Address address = addresses.get(0);
-                // Use the address as needed
-                latitudeF = (float) address.getLatitude();
-                longitudeF = (float) address.getLongitude();
-            } else {
-                // Display appropriate message when Geocoder services are not available
-                Toast.makeText(getActivity(), "Unable to geocode zip code", Toast.LENGTH_LONG).show();
+            Float latitudeF = sharedPreferences.getFloat("latitude", 0);
+            Float longitudeF = sharedPreferences.getFloat("longitude", 0);
+
+            //zip code location
+            final Geocoder geocoder = new Geocoder(getActivity());
+
+
+            try {
+                List<Address> addresses = geocoder.getFromLocationName(zip, 1);
+                if (addresses != null && !addresses.isEmpty()) {
+                    Address address = addresses.get(0);
+                    // Use the address as needed
+                    latitudeF = (float) address.getLatitude();
+                    longitudeF = (float) address.getLongitude();
+                } else {
+                    // Display appropriate message when Geocoder services are not available
+                    Toast.makeText(getActivity(), "Unable to geocode zip code", Toast.LENGTH_LONG).show();
+                }
+            } catch (IOException e) {
+                // handle exception
             }
-        } catch (IOException e) {
-            // handle exception
+
+
+            if (latitudeF == 0 || longitudeF == 0) {
+                //use default for huntington
+                latitudeF = 38.4192f;
+                longitudeF = -82.4452f;
+                Toast.makeText(getActivity().getApplicationContext(), "Default Location Used", Toast.LENGTH_SHORT).show();
+
+            }
+
+            prefsEditor.putString("zip", zip);
+            prefsEditor.putFloat("latitude", latitudeF);
+            prefsEditor.putFloat("longitude", longitudeF);
+            prefsEditor.apply();
+
+
+            ((MainActivity) getActivity()).getWeatherData(null);
+
+            //close keyboard
+            InputMethodManager inputManager = (InputMethodManager) getActivity().getSystemService(Context.INPUT_METHOD_SERVICE);
+
+            inputManager.hideSoftInputFromWindow(v.getWindowToken(), InputMethodManager.HIDE_NOT_ALWAYS);
         }
-
-
-        if (latitudeF == 0 || longitudeF == 0) {
-            //use default for huntington
-            latitudeF = 38.4192f;
-            longitudeF = -82.4452f;
-            Toast.makeText(getActivity().getApplicationContext(), "Default Location Used", Toast.LENGTH_SHORT).show();
-
-        }
-
-        prefsEditor.putString("zip", zip);
-        prefsEditor.putFloat("latitude", latitudeF);
-        prefsEditor.putFloat("longitude", longitudeF);
-        prefsEditor.apply();
-
-
-        ((MainActivity) getActivity()).getWeatherData(null);
     }
 }
