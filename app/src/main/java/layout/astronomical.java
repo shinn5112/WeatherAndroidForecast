1fcf6fb6110690e971bdc11aa9b6f34eb91bb6ac
Brandon <bmduke1997@gmail.com>
2017-04-30 21:51:20 -0400

    modify time

--- app/src/main/java/layout/astronomical.java
+++ app/src/main/java/layout/astronomical.java
@@ -21,6 +21,7 @@ import net.weatheraf.weatherandroidforecast.R;
 import org.json.JSONException;
 
 import java.text.SimpleDateFormat;
+import java.util.Arrays;
 import java.util.Date;
 
 import WeatherAPI.DailyDataPoint;
@@ -39,7 +40,7 @@ public class astronomical extends Fragment{
     private TextView moonPhase;
     private WeatherData weatherData;
     private ProgressBar pBar;
-    private ImageView moonImg;
+    private ImageView moonImg, sunImg;
 
     public astronomical() {
         // Required empty public constructor
@@ -59,6 +60,7 @@ public class astronomical extends Fragment{
         moonPhase = (TextView) view.findViewById(R.id.moonPhase);
         pBar = (ProgressBar) view.findViewById(R.id.circular_progress_bar);
         moonImg = (ImageView) view.findViewById(R.id.moonImg);
+        sunImg = (ImageView) view.findViewById(R.id.sunImg);
 
         try {
             weatherData = new WeatherData(sharedPreferences.getString("weatherData", ""));
@@ -79,8 +81,30 @@ public class astronomical extends Fragment{
         dateView.setText(sunRise[0] + " " + sunRise[1] + " " + sunRise[2]);
 
         //TODO moon rise and set
-        riseTime.setText(sunRise[3].substring(0, sunRise[3].length() - 3) + " " + sunRise[4]);
-        setTime.setText(sunSet[3].substring(0, sunSet[3].length() - 3) + " " + sunSet[4]);
+        String currentTime = new SimpleDateFormat("HH:mm").format(new Date());
+        String[] hour = sunSet[3].split(":");
+        int hr = Integer.valueOf(hour[0]) + 12;
+        String newHour = hr + ":" + hour[1];
+        System.out.println(currentTime.compareTo(newHour));
+
+        boolean night;
+        if (currentTime.compareTo(newHour) < 0) { //day
+            riseTime.setText(sunRise[3].substring(0, sunRise[3].length() - 3) + " " + sunRise[4]);
+            setTime.setText(sunSet[3].substring(0, sunSet[3].length() - 3) + " " + sunSet[4]);
+
+            sunImg.setImageResource(R.drawable.ic_wb_sunny_black_30dp);
+
+            night = false;
+        }
+        else { //night
+            riseTime.setText(sunSet[3].substring(0, sunSet[3].length() - 3) + " " + sunSet[4]); //sunset time from day
+
+            DailyDataPoint ddp2 = weatherData.getDaily().getDay(1);
+            String moonSet[] = ddp2.getSunriseTime().split(" ");
+            setTime.setText(moonSet[3].substring(0, moonSet[3].length() - 3) + " " + moonSet[4]); //sun rise time from next day
+
+            night = true;
+        }
 
         Double phase = ddp.getMoonPhase();
 
@@ -88,45 +112,66 @@ public class astronomical extends Fragment{
         //on a given night. Went two above and below quarterly values.
         String moon = "Moon Phase: ";
         moonImg.setColorFilter(Color.argb(255, 255, 255, 255)); // Set color of moon images, white
+        if (night) sunImg.setColorFilter(Color.argb(255, 255, 255, 255)); //TODO PROPER MOON COLOR
+        //else sunImg.setColorFilter(); //TODO set back to yellow
         if(phase <= 0.01){
             moon += "New Moon";
             moonImg.setImageResource(R.drawable.moon_new);
+            if (night) sunImg.setImageResource(R.drawable.moon_new);
         }
         else if(phase <= 0.22){
             moon += "Waxing Crescent";
             moonImg.setImageResource(R.drawable.moon_cresent);
+            if (night) sunImg.setImageResource(R.drawable.moon_cresent);
         }
         else if(phase <= 0.27){
             moon += "First Quarter";
             moonImg.setImageResource(R.drawable.moon_quarter);
+            if (night) sunImg.setImageResource(R.drawable.moon_quarter);
         }
         else if(phase <= 0.47){
             moon += "Waxing Gibbous";
             moonImg.setImageResource(R.drawable.moon_gibbous);
+            if (night) sunImg.setImageResource(R.drawable.moon_gibbous);
         }
         else if(phase <= 0.52){
             moon += "Full Moon";
             moonImg.setImageResource(R.drawable.moon_full);
+            if (night) sunImg.setImageResource(R.drawable.moon_full);
         }
         else if(phase <= 0.72){
             moon += "Waning Gibbous";
             moonImg.setImageResource(R.drawable.moon_gibbous);
             moonImg.setRotation(180.0f);
+            if (night) {
+                sunImg.setImageResource(R.drawable.moon_gibbous);
+                sunImg.setRotation(180.0f);
+            }
         }
         else if(phase <= 0.77){
             moon += "Last Quarter";
             moonImg.setImageResource(R.drawable.moon_quarter);
             moonImg.setRotation(180.0f);
+            if (night) {
+                sunImg.setImageResource(R.drawable.moon_quarter);
+                sunImg.setRotation(180.0f);
+            }
         }
         else{
             moon += "Waning Crescent " + phase;
             moonImg.setImageResource(R.drawable.moon_cresent);
             moonImg.setRotation(180.0f);
+            if (night) {
+                sunImg.setImageResource(R.drawable.moon_cresent);
+                moonImg.setRotation(180.0f);
+            }
         }
 
         moonPhase.setText(moon);
 
         int sunPosition = sunPosition();
+
+        System.out.println("THE SUN POS " + sunPosition);
 
         ObjectAnimator anim = ObjectAnimator.ofInt(pBar, "progress", 0, sunPosition);
         anim.setDuration(1000);
@@ -140,15 +185,30 @@ public class astronomical extends Fragment{
 
         String time = getDateStringFormatted();
         String hours = time.substring(12,14);
+        System.out.println("THE HR: "+ hours);
 
         int hr = Integer.parseInt(hours);
         int possibleRound = Integer.parseInt(time.substring(15,16));
 
-        String rise[] = riseTime.getText().toString().split(" ");
-        String set[] = setTime.getText().toString().split(" ");
+        String rise[];
+        String set[];
+
+        rise = riseTime.getText().toString().split(" ");
+        set = setTime.getText().toString().split(" ");
+
+
 
         String riseTimeHR = rise[0].substring(0, 1);
-        int riseHR = Integer.parseInt(riseTimeHR);
+        int riseHR;
+        if(rise[1].charAt(0) == 'P'){
+            riseHR = Integer.parseInt(riseTimeHR);
+            riseHR += 12;
+        }
+        else
+            riseHR = Integer.parseInt(riseTimeHR);
+
+        System.out.println("THE RISEE HR: "+ riseHR);
+
 
         int setHR = 0;
         String setTimeHR = set[0].substring(0, 1);
@@ -160,17 +220,27 @@ public class astronomical extends Fragment{
         else
             setHR = Integer.parseInt(setTimeHR);
 
+        System.out.println("SET HR " + setHR);
+
+
         if(hr > riseHR && possibleRound >=3)
             hr++;
 
-        if(hr > setHR || hr <= riseHR) {
+        if(hr > setHR || hr <= riseHR) { //night moon risen percent
+            System.out.println("RUN 224");
+            double sunPos = hr - riseHR;
+            sunPos *= (24.0/(double)(riseHR - setHR));
+            sunPos = ceil(sunPos);
+            sunPosition = (int) sunPos;
             return sunPosition;
         }
-        else if(hr == setHR){
+        if(hr == setHR){
             sunPosition = 24;
+            System.out.println("RUN 228");
             return sunPosition;
         }
         else {
+            System.out.println("RUN 232");
             double sunPos = hr - riseHR;
             sunPos *= (24.0/(double)(setHR - riseHR));
             sunPos = ceil(sunPos);
